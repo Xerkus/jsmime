b1d88492315e6b5606084287bfcdd01a9517d923
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-07-02 09:11:51 +1000

    Add changes needed to load as node module

--- jsmime.js
+++ jsmime.js
@@ -1,7 +1,21 @@
-define(function(require) {
-  return {
-    MimeParser: require('./mimeparser'),
-    headerparser: require('./headerparser'),
-    headeremitter: require('./headeremitter')
-  }
-});
+'use strict';
+(function () {
+    if (typeof define === 'function' && define.amd) {
+        define(function (require) {
+            return {
+                MimeParser: require('./mimeparser'),
+                headerparser: require('./headerparser'),
+                headeremitter: require('./headeremitter')
+            }
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        var requirejs = require('requirejs');
+        requirejs.config({
+            baseUrl: __dirname
+        });
+        exports.MimeParser = requirejs('./mimeparser');
+        exports.headerparser = requirejs('./headerparser');
+        exports.headeremitter = requirejs('./headeremitter')
+        exports.mimeutils = requirejs('./mimeutils')
+    }
+})();
